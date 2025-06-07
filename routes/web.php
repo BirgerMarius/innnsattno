55372ed7f26be7662f6a20933f048340b11b807a
ehtishamhussain <ehtishamhussain59@gmail.com>
2025-06-07 22:55:42 +0500

    Channels OUT: C More First C More Series History BBC Earth VOX Discovery Science
    Channels IN:
    DR1
    Nicelodeon,
    CNN International
    BBC News
    Al Jazzera English
    MTV

--- routes/web.php
+++ routes/web.php
@@ -53,10 +53,11 @@ Route::get('/print', function () {
     // tv2-nyhetskanalen, bbc-world-news, cnn-international, al-jazeera-english, cartoon-network, nickelodeon
 
     $response = Http::acceptJson()->get('https://tvguide.vg.no/backend/api/tv-schedule', [
-        'channels' => 'c-more-first,c-more-hits,c-more-series,nrk1,nrk2,nrk3,tv2-direkte,tv2-livsstil,tv2-sport-1,tv2-sport-2,tv2-zebra,tv3,tv3-plus,tvnorge,rex,investigation-discovery,history,national-geographic,discovery-channel,bbc-earth,mtv,fem,eurosport-norge,eurosport-1,vox,discovery-science',
+        'channels' => 'dr1,c-more-hits,nrk1,nrk2,nrk3,tv2-direkte,tv2-livsstil,tv2-sport-1,tv2-sport-2,tv2-zebra,tv3,tv3-plus,tvnorge,rex,investigation-discovery,national-geographic,discovery-channel,bbc-world-news,mtv,fem,eurosport-norge,eurosport-1,al-jazeera-english,nickelodeon',
         'date' => Carbon::parse(now())->format('Y-m-d'),
         'tz' => 'Europe/Oslo',
     ]);
+//    'channels' => 'dr1,c-more-hits,nicelodeon,nrk1,nrk2,nrk3,tv2-direkte,tv2-livsstil,tv2-sport-1,tv2-sport-2,tv2-zebra,tv3,tv3-plus,tvnorge,rex,investigation-discovery,history,national-geographic,discovery-channel,bbc-news,mtv,fem,eurosport-norge,eurosport-1,al-jazzera-english,mtv-00s,mtv-80s',
 
     if ($response->serverError()) {
         return "Innsatt.no klarer ikke hente TV-guide fra vg.no - dette kan være fordi siden er nede.";
@@ -77,11 +78,11 @@ Route::get('/print2', function () {
         'tz' => 'Europe/Oslo',
     ]);
 
-   
+
         $channels = json_decode($response, true);
         // dd($channels);
         return view('pdf2')->with(['channels' => $channels]);
-    
+
 
 });
 
