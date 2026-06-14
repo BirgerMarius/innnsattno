6ac936626fabfc9caa63f93f6466a9553764bb8a
Birger Marius Kristiansen <birger.marius@vikerkleiva.no>
2026-06-14 18:18:40 +0200

    Gjenopprettet root route

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,13 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
  */
+Route::get('/', function () {
+    return redirect('tv');
+});
+
+Route::get('/tv', function () {
+    return view('tv.guide');
+});
 
 Route::get('/tv', function () {
     return view('tv.guide');
