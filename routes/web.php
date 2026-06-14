523d22d30e13b6f4831d33fd35820f3ab09b27c6
Birger Marius Kristiansen <birger.marius@vikerkleiva.no>
2026-06-14 19:18:45 +0200

    Ryddet kanalliste og arkiverte gamle filer

--- routes/web.php
+++ routes/web.php
@@ -40,17 +40,53 @@ Route::get('/tv-guiden', function () {
 
 Route::get('/print', function () {
 
+   Route::get('/print', function () {
+
+    $channels = [
+        'nrk1',
+        'nrk2',
+        'nrk3',
+        'tv2-direkte',
+        'tv2-zebra',
+        'tvnorge',
+        'tv3',
+        'tv3-plus',
+
+        'tv2-sport-1',
+        'tv2-sport-2',
+        'eurosport-norge',
+        'eurosport-1',
+
+        'c-more-hits',
+        'tv2-livsstil',
+        'rex',
+        'fem',
+
+        'national-geographic',
+        'discovery-channel',
+        'investigation-discovery',
+
+        'bbc-world-news',
+        'al-jazeera-english',
+
+        'nickelodeon',
+
+        'dr1',
+        'mtv',
+    ];
+
     $response = Http::acceptJson()->get('https://tvguide.vg.no/backend/api/tv-schedule', [
-    'channels' => 'nrk1,nrk2,nrk3,tv2-direkte,tv2-zebra,tvnorge,tv3,tv3-plus,tv2-sport-1,tv2-sport-2,eurosport-norge,eurosport-1,c-more-hits,tv2-livsstil,rex,fem,national-geographic,discovery-channel,investigation-discovery,bbc-world-news,al-jazeera-english,nickelodeon,dr1,mtv',        'date' => Carbon::parse(now())->format('Y-m-d'),
+        'channels' => implode(',', $channels),
+        'date' => Carbon::parse(now())->format('Y-m-d'),
         'tz' => 'Europe/Oslo',
     ]);
 
     if ($response->serverError()) {
         return "Innsatt.no klarer ikke hente TV-guide fra vg.no - dette kan være fordi siden er nede.";
     } else {
-        $channels = json_decode($response, true);
-        // dd($channels);
-        return view('pdf')->with(['channels' => $channels]);
+        $tvChannels = json_decode($response, true);
+
+        return view('pdf')->with(['channels' => $tvChannels]);
     }
 
 });
