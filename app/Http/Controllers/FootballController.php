4295fb8a89de5cbb1a466c14707caa007777878d
BirgerMarius <78381777+BirgerMarius@users.noreply.github.com>
2026-06-19 11:38:27 +0200

    Vis lagnavn på fotballside

--- app/Http/Controllers/FootballController.php
+++ app/Http/Controllers/FootballController.php
@@ -6,17 +6,32 @@ use Illuminate\Support\Facades\Http;
 
 class FootballController extends Controller
 {
-    public function index()
-    {
-        $response = Http::get(
-            'https://api.sportsnext.schibsted.io/v1/vg/tournaments/seasons/7767/schedule'
-        );
+public function index()
+{
+    $response = Http::get(
+        'https://api.sportsnext.schibsted.io/v1/vg/tournaments/seasons/7767/schedule'
+    );
 
-        $data = $response->json();
-        dd(reset($data['participants']));
+    $data = $response->json();
 
-        return view('football.index', [
-            'data' => $data
-        ]);
+    $participants = $data['participants'];
+    $matches = [];
+
+    foreach ($data['events'] as $event) {
+
+        $homeId = $event['participantIds'][0] ?? null;
+        $awayId = $event['participantIds'][1] ?? null;
+
+        $matches[] = [
+            'date' => $event['startDate'] ?? '',
+            'group' => $event['tournament']['groupName'] ?? '',
+            'status' => $event['status']['type'] ?? '',
+            'home' => $participants[$homeId]['name'] ?? 'Ukjent',
+            'away' => $participants[$awayId]['name'] ?? 'Ukjent',
+        ];
     }
+
+    return view('football.index', [
+        'matches' => $matches
+    ]);
 }
--- resources/views/football/index.blade.php
+++ resources/views/football/index.blade.php
@@ -1,19 +1,21 @@
 <h1>⚽ Fotball-VM 2026</h1>
 
-<p>Antall kamper: {{ count($data['events']) }}</p>
+<p>Antall kamper: {{ count($matches) }}</p>
 
 <table border="1" cellpadding="5">
     <tr>
         <th>Dato</th>
+        <th>Kamp</th>
         <th>Gruppe</th>
         <th>Status</th>
     </tr>
 
-    @foreach($data['events'] as $event)
+    @foreach($matches as $match)
         <tr>
-            <td>{{ $event['startDate'] ?? '' }}</td>
-            <td>{{ $event['tournament']['groupName'] ?? '' }}</td>
-            <td>{{ $event['status']['type'] ?? '' }}</td>
+            <td>{{ $match['date'] }}</td>
+            <td>{{ $match['home'] }} - {{ $match['away'] }}</td>
+            <td>{{ $match['group'] }}</td>
+            <td>{{ $match['status'] }}</td>
         </tr>
     @endforeach
 
