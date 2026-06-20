71838f356c20a515b9799601b3d5c0bc5943bf5a
BirgerMarius <78381777+BirgerMarius@users.noreply.github.com>
2026-06-20 21:15:36 +0200

    Fjern gammel kampliste

--- resources/views/football/index.blade.php
+++ resources/views/football/index.blade.php
@@ -106,38 +106,3 @@
 <br>
 
 @endforeach
-
-<p>Antall kamper: {{ count($matches) }}</p>
-
-<table border="1" cellpadding="5">
-    <tr>
-        <th>Dato</th>
-        <th>Kamp</th>
-        <th>Resultat</th>
-        <th>Gruppe</th>
-        <th>Status</th>
-    </tr>
-
-    @foreach(array_slice($matches, 0, 20) as $match)
-        <tr>
-            <td>{{ $match['date'] }}</td>
-
-            <td>
-                {{ $match['home'] }} - {{ $match['away'] }}
-            </td>
-
-            <td>
-                @if($match['homeScore'] !== null)
-                    {{ $match['homeScore'] }} - {{ $match['awayScore'] }}
-                @else
-                    -
-                @endif
-            </td>
-
-            <td>{{ $match['group'] }}</td>
-
-            <td>{{ $match['status'] }}</td>
-        </tr>
-    @endforeach
-
-</table>
