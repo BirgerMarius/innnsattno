5447f1647332607b6d8d6d636d407e975ca2170b
BirgerMarius <78381777+BirgerMarius@users.noreply.github.com>
2026-06-23 20:38:57 +0200

    Fjern debug og rydd kampvisning

--- resources/views/football/index.blade.php
+++ resources/views/football/index.blade.php
@@ -65,25 +65,18 @@
 @else
 
 @if(!empty($match['homeFlagCode']))
-    <img src="https://flagcdn.com/24x18/{{ $match['homeFlagCode'] }}.png"
-         style="vertical-align:middle;">
+<img src="https://flagcdn.com/24x18/{{ $match['homeFlagCode'] }}.png" style="vertical-align:middle;">
 @endif
-
 {{ $match['home'] }}
-({{ $match['homeCountryCodeRaw'] }})
 
 -
 
 @if(!empty($match['awayFlagCode']))
-    <img src="https://flagcdn.com/24x18/{{ $match['awayFlagCode'] }}.png"
-         style="vertical-align:middle;">
+<img src="https://flagcdn.com/24x18/{{ $match['awayFlagCode'] }}.png" style="vertical-align:middle;">
 @endif
-
 {{ $match['away'] }}
-({{ $match['awayCountryCodeRaw'] }})
 
 @endif
-
 
 </td>
         </tr>
