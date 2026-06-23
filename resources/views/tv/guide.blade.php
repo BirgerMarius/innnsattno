e8ccb63fc59b1355b03c07fdb8240b24b9a696b7
BirgerMarius <78381777+BirgerMarius@users.noreply.github.com>
2026-06-23 18:56:12 +0200

    Startet felles Laravel-layout

--- resources/views/tv/guide.blade.php
+++ resources/views/tv/guide.blade.php
@@ -4,47 +4,6 @@
 
 @section('content')
 
-<!DOCTYPE html>
-<html lang="nb">
-
-<head>
-
-  
-    <meta charset="UTF-8">
-    <link rel="icon" type="image/png" href="/favicon.png">
-    <meta name="robots" content="noindex">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>INNSATT.NO</title>
-
-
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
-        integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.13.0/css/all.css"
-        integrity="sha384-IIED/eyOkM6ihtOiQsX2zizxFBphgnv1zbe1bKA+njdFzkr6cDNy16jfIKWu4FNH" crossorigin="anonymous">
-
-
-<link href="{{ asset('css/app.css') }}" rel="stylesheet">
-
-    <script type="text/javascript">
-        (function(c, l, a, r, i, t, y) {
-            c[a] = c[a] || function() {
-                (c[a].q = c[a].q || []).push(arguments)
-            };
-            t = l.createElement(r);
-            t.async = 1;
-            t.src = "https://www.clarity.ms/tag/" + i;
-            y = l.getElementsByTagName(r)[0];
-            y.parentNode.insertBefore(t, y);
-        })(window, document, "clarity", "script", "ekizv6kzel");
-    </script>
-
-</head>
-
-<body>
-
-
     <div class="container my-5">
 
 @php
@@ -282,7 +241,5 @@ $isEvenWeek = $weekNumber % 2 === 0;
 
 
 
-</body>
 
-</html>
 @endsection
