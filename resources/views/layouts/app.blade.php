15329910d0f50119bdd24370a569ed04404d8603
BirgerMarius <78381777+BirgerMarius@users.noreply.github.com>
2026-06-23 18:58:58 +0200

    Flyttet felles CSS til Laravel-layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,6 +8,20 @@
 
     <link rel="icon" type="image/png" href="{{ asset('favicon.png') }}">
 
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+<link rel="stylesheet"
+      href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+      integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOENV6gBiFeWPGFN9MuhOf23Q9Ifjh"
+      crossorigin="anonymous">
+
+<link rel="stylesheet"
+      href="https://pro.fontawesome.com/releases/v5.13.0/css/all.css"
+      integrity="sha384-IIED/eyOkM6ihtOiQsX2zizxFBphgnv1zbelbKA+njdFzkr6cDNy16jTiKWu4FNH"
+      crossorigin="anonymous">
+
+<link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
     @yield('head')
 
     <!-- Microsoft Clarity -->
