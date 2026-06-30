c7d7273f90f2aa1af3b5ff0c15c47b84c6e92735
BirgerMarius <78381777+BirgerMarius@users.noreply.github.com>
2026-06-30 11:39:06 +0200

    Legg til Microsoft Clarity i felles layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,21 +4,21 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <title>@yield('title', 'Innsatt.no')</title>
+    <title>@yield('title', 'INNSATT.NO')</title>
 
     <link rel="icon" type="image/png" href="{{ asset('favicon.png') }}">
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
-<link rel="stylesheet"
-href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <link rel="stylesheet"
+          href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
 
-<link rel="stylesheet"
-href="https://pro.fontawesome.com/releases/v5.13.0/css/all.css">
+    <link rel="stylesheet"
+          href="https://pro.fontawesome.com/releases/v5.13.0/css/all.css">
 
-<link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
-    @yield('head')
+    @stack('head')
 
     <!-- Microsoft Clarity -->
     <script type="text/javascript">
@@ -31,12 +31,15 @@ href="https://pro.fontawesome.com/releases/v5.13.0/css/all.css">
             t.src="https://www.clarity.ms/tag/"+i;
             y=l.getElementsByTagName(r)[0];
             y.parentNode.insertBefore(t,y);
-        })(window, document, "clarity", "script", "DIN_CLARITY_ID");
+        })(window, document, "clarity", "script", "xf2dio3725");
     </script>
+
 </head>
 <body>
 
     @yield('content')
+
+    @stack('scripts')
 
 </body>
 </html>
