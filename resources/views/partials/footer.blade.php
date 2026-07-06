f28a5bd893dc83b033a870cc830fc66556483c0e
BirgerMarius <78381777+BirgerMarius@users.noreply.github.com>
2026-07-06 23:53:50 +0200

    Flytt footer til felles partial

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,17 @@
+<div class="site-footer bg-dark text-white text-center p-3 mt-4 rounded">
+
+    <h5>INNSATT.NO</h5>
+
+    <p class="mb-2">
+        Innsatt.no administreres av fengselsbetjent Birger Marius Kristiansen.
+        Ta gjerne kontakt med forslag til forbedringer, feilrettinger eller andre innspill.
+    </p>
+
+    <p class="mb-0">
+        Kontakt:
+        <a href="mailto:innsatt@innsatt.no" class="text-light">
+            innsatt@innsatt.no
+        </a>
+    </p>
+
+</div>
--- resources/views/tv/guide.blade.php
+++ resources/views/tv/guide.blade.php
@@ -220,20 +220,7 @@ $isEvenWeek = $weekNumber % 2 === 0;
     {{ trim(shell_exec('git log -1 --format="%cd" --date=format:"%d.%m.%Y"')) }}
 </p>
 
-<div class="bg-dark text-white text-center p-3 mt-4 rounded">
-        <h5>INNSATT.NO</h5>
-
-    <p class="mb-2">
-        Innsatt.no administreres av fengselsbetjent Birger Marius Kristiansen. Ta gjerne kontakt med forslag til forbedringer, feilrettinger eller andre innspill!
-    </p>
-
-    <p class="mb-0">
-        Kontakt:
-        <a href="mailto:innsatt@innsatt.no" class="text-light">
-            innsatt@innsatt.no
-        </a>
-    </p>
-</div>
+@include('partials.footer')
 
 
     </div>
