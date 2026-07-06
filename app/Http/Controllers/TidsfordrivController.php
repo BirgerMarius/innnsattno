64d661787fda8be00b2a2d701fb92e9c3567da19
BirgerMarius <78381777+BirgerMarius@users.noreply.github.com>
2026-07-06 18:25:52 +0200

    Legg til YouDoSudoku API-metode

--- app/Http/Controllers/TidsfordrivController.php
+++ app/Http/Controllers/TidsfordrivController.php
@@ -12,13 +12,31 @@ class TidsfordrivController extends Controller
         return view('tidsfordriv.index');
     }
 
-   public function printSudoku(Request $request)
-{
-    $sudoku = $this->getSudoku(
-        $request->difficulty,
-        $request->has('solution')
-    );
+    public function printSudoku(Request $request)
+    {
+        $sudoku = $this->getSudoku(
+            $request->difficulty,
+            $request->has('solution')
+        );
 
-    dd($sudoku);
-}
+        dd($sudoku);
+    }
+
+    private function getSudoku(string $difficulty, bool $solution = true)
+    {
+        $response = Http::withHeaders([
+            'Content-Type' => 'application/json',
+            'x-api-key' => env('YOUDOSUDOKU_API_KEY'),
+        ])->post('https://youdosudoku.com/api/', [
+            'difficulty' => $difficulty,
+            'solution' => $solution,
+            'array' => false,
+        ]);
+
+        if (!$response->successful()) {
+            return null;
+        }
+
+        return $response->json();
+    }
 }
